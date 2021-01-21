42f48f9303867b956cc54882761542513da03682
Matthias Steiner <m@kjpjm.de>
2021-01-21 21:50:16 +0100

    inital commit

---
+++ Buchstabenzählen21-01-2021/array.js
@@ -0,0 +1,17 @@
+let namen = ["Albert", "Barbara", "Charly"];
+
+//
+// Schleifen um Array durchzugehen
+//
+
+//langeform
+let i = 0;
+while (i < namen.length) {
+  console.log(namen[i]);
+  i++; // i = i+1;
+}
+
+//kurzform
+for (let i = 0; i < namen.length; i++) {
+  console.log(namen[i]);
+}
