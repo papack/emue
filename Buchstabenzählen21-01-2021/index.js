f18c4bbf068ce4a2b8b19d3a0dc4ad600e528f5a
Matthias Steiner <m@kjpjm.de>
2021-01-21 22:39:03 +0100

    erste version

---
+++ Buchstabenzählen21-01-2021/index.js
@@ -0,0 +1,142 @@
+let text =
+  "In den letzten Tagen seiner Präsidentschaft distanzieren sich Geschäftspartner und Unterstützer von Donald Trump. Damit könnte sein Unternehmen in ernsthafte Schwierigkeiten geraten. Mit dem Ende seiner Präsidentschaft wartet auf Donald Trump eine gewaltige Herausforderung: Er muss sich um sein Firmengeflecht kümmern. Dort ist die Lage derzeit unerfreulich. Die Geschäfte laufen nicht gut, Kredite in Millionenhöhe müssen zurückgezahlt werden, juristischer Ärger droht - und Unternehmen und republikanische Politiker gehen zu Trump auf Distanz, nachdem er maßgeblich zum Sturm auf das Kapitol in Washington beigetragen hatte. Das Geschäftsmodell der Trump Organization basiert auf einer Sache: dem Namen Trump. Er steht für einen zur Schau gestellten, golden glitzernden Luxus für eine Klientel, die sich mit diesem Image schmücken will. Neben seinen Hotels und Golfplätzen setzt Trump darauf, seinen Namen zu lizenzieren. Er stellt diese Marke für die Projekte anderer zur Verfügung und kassiert dafür Lizenzgebühren. Außerdem verbreitet Trump von sich das Bild eines überaus erfolgreichen Mannes. Spätestens mit der Niederlage bei den Präsidentschaftswahlen und dem Beginn des zweiten Amtsenthebungsverfahrens ist das erheblich angekratzt. Künftig dürfte es Trump also schwererfallen, sein Geschäftsmodell aufrechtzuerhalten. Das liegt nicht nur daran, dass seine Marke an Glamour - und damit an Wert - verloren hat. Vielmehr wenden sich selbst langjährige Geschäftspartner von ihm ab, etwa die Deutsche Bank.";
+
+let anzahlA = 0;
+let anzahlB = 0;
+let anzahlC = 0;
+let anzahlD = 0;
+let anzahlE = 0;
+let anzahlF = 0;
+let anzahlG = 0;
+let anzahlH = 0;
+let anzahlI = 0;
+let anzahlJ = 0;
+let anzahlK = 0;
+let anzahlL = 0;
+let anzahlM = 0;
+let anzahlN = 0;
+let anzahlO = 0;
+let anzahlP = 0;
+let anzahlQ = 0;
+let anzahlR = 0;
+let anzahlS = 0;
+let anzahlT = 0;
+let anzahlU = 0;
+let anzahlV = 0;
+let anzahlW = 0;
+let anzahlX = 0;
+let anzahlY = 0;
+let anzahlZ = 0;
+
+for (let i = 0; i < text.length; i++) {
+  //console.log("was soll ich damit machen?: " + i);
+
+  //console.log("BuchstabeNr." + i);
+  //console.log(text);
+
+  if (text[i].toUpperCase() === "A") {
+    anzahlA++;
+  }
+  if (text[i].toUpperCase() === "B") {
+    anzahlB++;
+  }
+  if (text[i].toUpperCase() === "C") {
+    anzahlC++;
+  }
+  if (text[i].toUpperCase() === "D") {
+    anzahlD++;
+  }
+  if (text[i].toUpperCase() === "E") {
+    anzahlE++;
+  }
+  if (text[i].toUpperCase() === "F") {
+    anzahlF++;
+  }
+  if (text[i].toUpperCase() === "G") {
+    anzahlG++;
+  }
+  if (text[i].toUpperCase() === "H") {
+    anzahlH++;
+  }
+  if (text[i].toUpperCase() === "I") {
+    anzahlI++;
+  }
+  if (text[i].toUpperCase() === "J") {
+    anzahlJ++;
+  }
+  if (text[i].toUpperCase() === "K") {
+    anzahlK++;
+  }
+  if (text[i].toUpperCase() === "L") {
+    anzahlL++;
+  }
+  if (text[i].toUpperCase() === "M") {
+    anzahlM++;
+  }
+  if (text[i].toUpperCase() === "N") {
+    anzahlN++;
+  }
+  if (text[i].toUpperCase() === "O") {
+    anzahlO++;
+  }
+  if (text[i].toUpperCase() === "P") {
+    anzahlP++;
+  }
+  if (text[i].toUpperCase() === "Q") {
+    anzahlQ++;
+  }
+  if (text[i].toUpperCase() === "R") {
+    anzahlR++;
+  }
+  if (text[i].toUpperCase() === "S") {
+    anzahlS++;
+  }
+  if (text[i].toUpperCase() === "T") {
+    anzahlT++;
+  }
+  if (text[i].toUpperCase() === "U") {
+    anzahlU++;
+  }
+  if (text[i].toUpperCase() === "V") {
+    anzahlV++;
+  }
+  if (text[i].toUpperCase() === "W") {
+    anzahlW++;
+  }
+  if (text[i].toUpperCase() === "X") {
+    anzahlX++;
+  }
+  if (text[i].toUpperCase() === "Y") {
+    anzahlY++;
+  }
+  if (text[i].toUpperCase() === "Z") {
+    anzahlZ++;
+  }
+}
+
+console.log("A:" + anzahlA);
+console.log("B:" + anzahlB);
+console.log("C:" + anzahlC);
+console.log("D:" + anzahlD);
+console.log("E:" + anzahlE);
+console.log("F:" + anzahlF);
+console.log("G:" + anzahlG);
+console.log("H:" + anzahlH);
+console.log("I:" + anzahlI);
+console.log("J:" + anzahlJ);
+console.log("K:" + anzahlK);
+console.log("L:" + anzahlL);
+console.log("M:" + anzahlM);
+console.log("N:" + anzahlN);
+console.log("O:" + anzahlO);
+console.log("P:" + anzahlP);
+console.log("Q:" + anzahlQ);
+console.log("R:" + anzahlR);
+console.log("S:" + anzahlS);
+console.log("T:" + anzahlT);
+console.log("U:" + anzahlU);
+console.log("V:" + anzahlV);
+console.log("W:" + anzahlW);
+console.log("X:" + anzahlX);
+console.log("Y:" + anzahlY);
+console.log("Z:" + anzahlZ);
