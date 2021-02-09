64119a9ce18f528e81c26584027d9e8a32e3ab9e
Matthias Steiner <m@kjpjm.de>
2021-02-09 22:29:10 +0100

    add comments

--- js-spicker.js
+++ js-spicker.js
@@ -1,26 +1,38 @@
+//
+// While
+//
+
 let i = 0;
 while (i < 100) {
   console.log(i);
   i++;
 }
 
+//
+// For
+//
+
 for (let i = 0; i < 100; i = i + 1) {
   console.log(i);
 }
 
-if (Bedingung) {
-    Dann
+//
+// IF
+//
+
+if (/*Bedingung*/) {
+    //Dann
 }
 else {
-    wenn nicht zutrifft
+    //wenn nicht zutrifft
 }
 
-1 == 1 wäre true
-1 == 2 wäre false
+//1 == 1 wäre true
+//1 == 2 wäre false
 
-1 != 2 wäre true
-1 1= 1 wäre false
+//1 != 2 wäre true
+//1 1= 1 wäre false
 
 
-!true wäre false
-!false wäre true
+//!true wäre false
+//!false wäre true
