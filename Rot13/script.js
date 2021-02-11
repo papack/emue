25aede4711ffc978b6e50d204f324235ce1d5014
Matthias Steiner <m@kjpjm.de>
2021-02-11 23:55:33 +0100

    Button anklicken + Inhalt übernehmen

---
+++ Rot13/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function handleButtonClick() {
+  const original = document.getElementById("original");
+  const kodiert = document.getElementById("kodiert");
+
+  kodiert.value = original.value;
+}
